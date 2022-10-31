0ecbeb44c1dbdbaa794ab23d8745b32ce145cf1e
Daniel Kenney <danielkenney99@yahoo.com>
2022-10-31 12:12:48 -0700

    mathematical functions and operation implemented

---
+++ script.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(num1, operator, num2) {
+  if (operator == "+") {
+    add(num1, num2);
+  } else if (operator == "-") {
+    subtract(num1, num2);
+  } else if (operator == "*") {
+    multiply(num1, num2);
+  } else if (operator == "/") {
+    divide(num1, num2);
+  }
+}
